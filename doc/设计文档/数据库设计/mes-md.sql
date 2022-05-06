0d6b1b20ba589f94d215182c8125dbe4dcb28729
JinLu.Yin <411641505@qq.com>
2022-05-06 17:09:27 +0800

    数据库设计文档

---
+++ doc/设计文档/数据库设计/mes-md.sql
@@ -0,0 +1,181 @@
+-- ----------------------------
+-- 1、物料产品表
+-- ----------------------------
+drop table if exists md_item;
+create table md_item (
+  item_id           bigint(20)      not null auto_increment    comment '产品物料ID',
+  item_code         varchar(64)     not null                   comment '产品物料编码',
+  item_name         varchar(255)    not null                   comment '产品物料名称',
+  specification     varchar(500)    default null               comment '规格型号',
+  unit_of_measure   varchar(64)     not null                   comment '单位',
+  item_or_product   varchar(20)     not null                   comment '产品物料标识',
+  item_type_id      bigint(20)      default 0                  comment '物料类型ID',
+  item_type_code    varchar(64)     default ''                 comment '物料类型编码',
+  item_type_name    varchar(255)    default ''                 comment '物料类型名称',
+  enable_flag       char(1)         default 'Y' not null       comment '是否启用',
+  safe_stock_flag   char(1)         default 'N' not null       comment '是否设置安全库存',
+  min_stock         double(12,4)    default 0                  comment '最低库存量',
+  max_stock         double(12,4)    default 0                  comment '最大库存量',
+  remark            varchar(500)    default ''                 comment '备注',
+  attr1             varchar(64)     default null               comment '预留字段1',
+  attr2             varchar(255)     default null               comment '预留字段2',
+  attr3             int(11)         default 0                  comment '预留字段3',
+  attr4             int(11)         default 0                  comment '预留字段4',
+  create_by         varchar(64)     default ''                 comment '创建者',
+  create_time 	    datetime                                   comment '创建时间',
+  update_by         varchar(64)     default ''                 comment '更新者',
+  update_time       datetime                                   comment '更新时间',
+  primary key (item_id)
+) engine=innodb auto_increment=200 comment = '物料产品表';
+
+-- ----------------------------
+-- 2、物料产品分类表
+-- ----------------------------
+drop table if exists md_item_type;
+create table md_item_type (
+  item_type_id      bigint(20)      not null auto_increment    comment '产品物料类型ID',
+  item_type_code    varchar(64)     not null                   comment '产品物料类型编码',
+  item_type_name    varchar(255)    not null                   comment '产品物料类型名称',
+  parent_type_id    bigint          default 0 not null         comment '父类型ID',
+  ancestors         varchar(255)    not null                   comment '所有层级父节点',
+  item_or_product   varchar(20)     not null                   comment '产品物料标识',
+  order_num         int(11)         default 1                  comment '排列顺序',
+  enable_flag       char(1)         default 'Y' not null       comment '是否启用',
+  remark            varchar(500)    default ''                 comment '备注',
+  attr1             varchar(64)     default null               comment '预留字段1',
+  attr2             varchar(255)     default null               comment '预留字段2',
+  attr3             int(11)         default 0                  comment '预留字段3',
+  attr4             int(11)         default 0                  comment '预留字段4',
+  create_by         varchar(64)     default ''                 comment '创建者',
+  create_time 	    datetime                                   comment '创建时间',
+  update_by         varchar(64)     default ''                 comment '更新者',
+  update_time       datetime                                   comment '更新时间',
+  primary key (item_type_id)
+) engine=innodb auto_increment=200 comment = '物料产品分类表';
+
+-- ----------------------------
+-- 3、产品BOM关系表
+-- ----------------------------
+drop table if exists md_product_bom;
+create table md_product_bom (
+  bom_id            bigint(20)      not null auto_increment    comment '流水号',
+  item_id           bigint(20)      not null                   comment '物料产品ID',
+  bom_item_id       bigint(20)      not null                   comment 'BOM物料ID',
+  bom_item_code     varchar(64)     not null                   comment 'BOM物料编码',
+  bom_item_name     varchar(255)    not null                   comment 'BOM物料名称',
+  bom_item_spec     varchar(500)                               comment 'BOM物料规格',
+  unit_of_measure   varchar(64)     not null                   comment 'BOM物料单位',
+  quantity          double(12,4)    default 0 not null         comment '物料使用比例',
+  enable_flag       char(1)         default 'Y' not null       comment '是否启用',
+  remark            varchar(500)    default ''                 comment '备注',
+  attr1             varchar(64)     default null               comment '预留字段1',
+  attr2             varchar(255)     default null               comment '预留字段2',
+  attr3             int(11)         default 0                  comment '预留字段3',
+  attr4             int(11)         default 0                  comment '预留字段4',
+  create_by         varchar(64)     default ''                 comment '创建者',
+  create_time 	    datetime                                   comment '创建时间',
+  update_by         varchar(64)     default ''                 comment '更新者',
+  update_time       datetime                                   comment '更新时间',
+  primary key (bom_id)
+) engine=innodb auto_increment=200 comment = '产品BOM关系表';
+
+
+-- ----------------------------
+-- 4、供应商表
+-- ----------------------------
+drop table if exists md_vendor;
+create table md_vendor (
+  vendor_id         bigint(20)      not null auto_increment    comment '供应商ID',
+  vendor_code       varchar(64)     not null                   comment '供应商编码',
+  vendor_name       varchar(255)    not null                   comment '供应商名称',
+  vendor_nick       varchar(255)                               comment '供应商简称',
+  vendor_en         varchar(255)                               comment '供应商英文名称',
+  vendor_des        varchar(500)                               comment '供应商简介',  
+  vendor_logo       varchar(255)                               comment '供应商LOGO地址',  
+  vendor_level      varchar(64)                                comment '供应商等级',  
+  vendor_score      int(11)                                    comment '供应商评分',
+  address           varchar(500)                               comment '供应商地址',
+  website           varchar(255)                               comment '供应商官网地址',
+  email             varchar(255)                               comment '供应商邮箱地址',
+  tel               varchar(64)                                comment '供应商电话',
+  contact1          varchar(64)                                comment '联系人1',
+  contact1_tel      varchar(64)                                comment '联系人1-电话',
+  contact1_email    varchar(255)                               comment '联系人1-邮箱',
+  contact2          varchar(64)                                comment '联系人2',
+  contact2_tel      varchar(64)                                comment '联系人2-电话',
+  contact2_email    varchar(255)                               comment '联系人2-邮箱',
+  credit_code       varchar(64)                                comment '统一社会信用代码',  
+  enable_flag       char(1)         default 'Y' not null       comment '是否启用',
+  remark            varchar(500)    default ''                 comment '备注',
+  attr1             varchar(64)     default null               comment '预留字段1',
+  attr2             varchar(255)     default null               comment '预留字段2',
+  attr3             int(11)         default 0                  comment '预留字段3',
+  attr4             int(11)         default 0                  comment '预留字段4',
+  create_by         varchar(64)     default ''                 comment '创建者',
+  create_time 	    datetime                                   comment '创建时间',
+  update_by         varchar(64)     default ''                 comment '更新者',
+  update_time       datetime                                   comment '更新时间',
+  primary key (vendor_id)
+) engine=innodb auto_increment=200 comment = '供应商表';
+
+
+-- ----------------------------
+-- 5、客户表
+-- ----------------------------
+drop table if exists md_client;
+create table md_client (
+  client_id         bigint(20)      not null auto_increment    comment '客户ID',
+  client_code       varchar(64)     not null                   comment '客户编码',
+  client_name       varchar(255)    not null                   comment '客户名称',
+  client_nick       varchar(255)                               comment '客户简称',
+  client_en         varchar(255)                               comment '客户英文名称',
+  client_des        varchar(500)                               comment '客户简介',  
+  client_logo       varchar(255)                               comment '客户LOGO地址',  
+  client_type       varchar(64)     default 'ENTERPRISE'       comment '客户类型',  
+  address           varchar(500)                               comment '客户地址',
+  website           varchar(255)                               comment '客户官网地址',
+  email             varchar(255)                               comment '客户邮箱地址',
+  tel               varchar(64)                                comment '客户电话',
+  contact1          varchar(64)                                comment '联系人1',
+  contact1_tel      varchar(64)                                comment '联系人1-电话',
+  contact1_email    varchar(255)                               comment '联系人1-邮箱',
+  contact2          varchar(64)                                comment '联系人2',
+  contact2_tel      varchar(64)                                comment '联系人2-电话',
+  contact2_email    varchar(255)                               comment '联系人2-邮箱',
+  credit_code       varchar(64)                                comment '统一社会信用代码',  
+  enable_flag       char(1)         default 'Y' not null       comment '是否启用',
+  remark            varchar(500)    default ''                 comment '备注',
+  attr1             varchar(64)     default null               comment '预留字段1',
+  attr2             varchar(255)     default null               comment '预留字段2',
+  attr3             int(11)         default 0                  comment '预留字段3',
+  attr4             int(11)         default 0                  comment '预留字段4',
+  create_by         varchar(64)     default ''                 comment '创建者',
+  create_time 	    datetime                                   comment '创建时间',
+  update_by         varchar(64)     default ''                 comment '更新者',
+  update_time       datetime                                   comment '更新时间',
+  primary key (client_id)
+) engine=innodb auto_increment=200 comment = '客户表';
+
+-- ----------------------------
+-- 4、单位表
+-- ----------------------------
+drop table if exists md_unit_measure;
+create table md_unit_measure (
+  measure_id         bigint(20)      not null auto_increment    comment '单位ID',
+  measure_code       varchar(64)     not null                   comment '单位编码',
+  measure_name       varchar(255)    not null                   comment '单位名称',
+  primary_flag       char(1)         default 'Y' not null       comment '是否是主单位',
+  primary_id         bigint(20)                                 comment '主单位ID',
+  change_rate        double(12,4)                               comment '与主单位换算比例',  
+  enable_flag       char(1)         default 'Y' not null        comment '是否启用',
+  remark            varchar(500)    default ''                  comment '备注',
+  attr1             varchar(64)     default null                comment '预留字段1',
+  attr2             varchar(255)     default null               comment '预留字段2',
+  attr3             int(11)         default 0                   comment '预留字段3',
+  attr4             int(11)         default 0                   comment '预留字段4',
+  create_by         varchar(64)     default ''                 comment '创建者',
+  create_time 	    datetime                                   comment '创建时间',
+  update_by         varchar(64)     default ''                 comment '更新者',
+  update_time       datetime                                   comment '更新时间',
+  primary key (measure_id)
+) engine=innodb auto_increment=200 comment = '单位表';
---
+++ doc/设计文档/数据库设计/mes_sys.sql
@@ -0,0 +1,83 @@
+-- ----------------------------
+-- 1、编码生成规则表
+-- ----------------------------
+drop table if exists sys_auto_code_rule;
+create table sys_auto_code_rule (
+  rule_id           bigint(20)      not null auto_increment    comment '规则ID',
+  rule_code         varchar(64)     not null                   comment '规则编码',
+  rule_name         varchar(255)    not null                   comment '规则名称',
+  rule_desc     	varchar(500)    default null               comment '描述',
+  max_length        int(11)                                    comment '最大长度',
+  is_padded         char(1)         not null                   comment '是否补齐',
+  padded_char       varchar(20)          	                   comment '补齐字符',
+  padded_method     char(1)         default 'L'                comment '补齐方式',
+  enable_flag       char(1)         default 'Y' not null       comment '是否启用',
+  remark            varchar(500)    default ''                 comment '备注',
+  attr1             varchar(64)     default null               comment '预留字段1',
+  attr2             varchar(255)     default null              comment '预留字段2',
+  attr3             int(11)         default 0                  comment '预留字段3',
+  attr4             int(11)         default 0                  comment '预留字段4',
+  create_by         varchar(64)     default ''                 comment '创建者',
+  create_time 	    datetime                                   comment '创建时间',
+  update_by         varchar(64)     default ''                 comment '更新者',
+  update_time       datetime                                   comment '更新时间',
+  primary key (rule_id)
+) engine=innodb auto_increment=200 comment = '编码生成规则表';
+
+
+-- ----------------------------
+-- 2、编码生成规则组成表
+-- ----------------------------
+drop table if exists sys_auto_code_part;
+create table sys_auto_code_part (
+  part_id           bigint(20)      not null auto_increment    comment '分段ID',
+  rule_id           bigint(20)      not null                   comment '规则ID',
+  part_index        int(11)         not null                   comment '分段序号',
+  part_type         varchar(20)     not null                   comment '分段类型，INPUTCHAR：输入字符，NOWDATE：当前日期时间，FIXCHAR：固定字符，SERIALNO：流水号',
+  part_code			varchar(64)                                comment '分段编号',
+  part_name         varchar(255)                               comment '分段名称',
+  part_length       int(11)         not null                   comment '分段长度',
+  date_formart      varchar(20)          	                   comment '日期时间格式',
+  input_character   varchar(64)                                comment '输入字符',
+  fix_character     varchar(64)                                comment '固定字符',
+  seria_start_no    int(11)         					       comment '流水号起始值',
+  seria_step        int(11)         					       comment '流水号步长',
+  seria_now_no      int(11)         					       comment '流水号当前值',
+  cycle_flag        char(1)         					       comment '流水号是否循环',
+  cycle_method      varchar(20)     					       comment '循环方式，YEAR：按年，MONTH：按月，DAY：按天，HOUR：按小时，MINITE：按分钟，OTHER：按传入字符变',
+  remark            varchar(500)    default ''                 comment '备注',
+  attr1             varchar(64)     default null               comment '预留字段1',
+  attr2             varchar(255)     default null              comment '预留字段2',
+  attr3             int(11)         default 0                  comment '预留字段3',
+  attr4             int(11)         default 0                  comment '预留字段4',
+  create_by         varchar(64)     default ''                 comment '创建者',
+  create_time 	    datetime                                   comment '创建时间',
+  update_by         varchar(64)     default ''                 comment '更新者',
+  update_time       datetime                                   comment '更新时间',
+  primary key (part_id)
+) engine=innodb auto_increment=200 comment = '编码生成规则组成表';
+
+
+-- ----------------------------
+-- 3、编码生成记录表
+-- ----------------------------
+drop table if exists sys_auto_code_result;
+create table sys_auto_code_result (
+  code_id           bigint(20)      not null auto_increment    comment '记录ID',
+  rule_id           bigint(20)      not null                   comment '规则ID',
+  gen_date          varchar(20)     not null                   comment '生成日期时间',
+  gen_index         int(11)                                    comment '最后产生的序号',
+  last_result		varchar(64)                                comment '最后产生的值',
+  last_serial_no    int(11)                                    comment '最后产生的流水号',
+  last_input_char   varchar(64)                                comment '最后传入的参数',
+  remark            varchar(500)    default ''                 comment '备注',
+  attr1             varchar(64)     default null               comment '预留字段1',
+  attr2             varchar(255)    default null               comment '预留字段2',
+  attr3             int(11)         default 0                  comment '预留字段3',
+  attr4             int(11)         default 0                  comment '预留字段4',
+  create_by         varchar(64)     default ''                 comment '创建者',
+  create_time 	    datetime                                   comment '创建时间',
+  update_by         varchar(64)     default ''                 comment '更新者',
+  update_time       datetime                                   comment '更新时间',
+  primary key (code_id)
+) engine=innodb auto_increment=200 comment = '编码生成记录表';
