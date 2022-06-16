65630637b9fd6c90e558538c88b76f4f483bd504
JinLu.Yin <411641505@qq.com>
2022-06-16 20:37:52 +0800

    设备点检保养项目

--- doc/设计文档/数据库设计/mes-dv.sql
+++ doc/设计文档/数据库设计/mes-dv.sql
@@ -51,3 +51,27 @@ create table dv_machinery (
   primary key (machinery_id)
 ) engine=innodb auto_increment=200 comment = '设备表';
 
+
+-- ----------------------------
+-- 3、设备点检保养项目表
+-- ----------------------------
+drop table if exists dv_subject;
+create table dv_subject (
+  subject_id                  bigint(20)      not null auto_increment    comment '项目ID',
+  subject_code                varchar(64)     not null                   comment '项目编码',
+  subject_name                varchar(255)                               comment '项目名称',
+  subject_type                varchar(64)     default 0                  comment '项目类型',
+  subject_content             varchar(500)    not null                   comment '项目内容',
+  subject_standard            varchar(255)                               comment '标准',
+  enable_flag                 char(1)         default 'Y' not null       comment '是否启用',
+  remark                      varchar(500)    default ''                 comment '备注',
+  attr1                       varchar(64)     default null               comment '预留字段1',
+  attr2                       varchar(255)    default null               comment '预留字段2',
+  attr3                       int(11)         default 0                  comment '预留字段3',
+  attr4                       int(11)         default 0                  comment '预留字段4',
+  create_by                   varchar(64)     default ''                 comment '创建者',
+  create_time 	              datetime                                   comment '创建时间',
+  update_by                   varchar(64)     default ''                 comment '更新者',
+  update_time                 datetime                                   comment '更新时间',
+  primary key (subject_id)
+) engine=innodb auto_increment=200 comment = '设备点检保养项目表';
