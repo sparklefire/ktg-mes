bcb724158a05e55ef943919e7c8985e155d6fec4
JinLu.Yin <411641505@qq.com>
2022-09-13 19:34:14 +0800

    设备分类根节点数据初始化。

--- doc/实施文档/inital.sql
+++ doc/实施文档/inital.sql
@@ -2,6 +2,10 @@
 insert into md_item_type(item_type_code,item_type_name,parent_type_id,ancestors,item_or_product,order_num,enable_flag 
 )values ('ITEM_TYPE_0000','物料产品分类',0,'0','PRODUCT',1,'Y');
 
+
+insert into dv_machinery_type(machinery_type_code,machinery_type_name,parent_type_id,ancestors,enable_flag)
+values ('M_TYPE_000','设备分类',0,'0','Y'); 
+
 --初始化一个虚拟的线边库
 insert into wm_warehouse (warehouse_code,warehouse_name,location,area,charge) values('XBK_VIRTUAL','线边库-虚拟',null,-1,null);
 
