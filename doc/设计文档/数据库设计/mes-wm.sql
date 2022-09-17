b03dd0d13a9534e7fbdf3f281f5793f43efe116a
JinLu.Yin <411641505@qq.com>
2022-09-17 23:11:47 +0800

    添加物料消耗相关表

--- doc/设计文档/数据库设计/mes-wm.sql
+++ doc/设计文档/数据库设计/mes-wm.sql
@@ -472,9 +472,74 @@ create table wm_rt_issue_line (
 
 
 
+-- ----------------------------
+-- 10、物料消耗记录表
+-- ----------------------------
+drop table if exists wm_item_consume;
+create table wm_item_consume (
+  record_id             bigint(20)      not null auto_increment     comment '记录ID',
+  workorder_id          bigint(20)                                  comment '生产工单ID',
+  workorder_code        varchar(64)                                 comment '生产工单编码', 
+  workorder_name        varchar(255)                                comment '生产工单名称',
+  task_id               bigint(20)                                  comment '生产任务ID',
+  task_code             varchar(64)                                 comment '生产任务编号',
+  task_name             varchar(255)                                comment '生产任务名称',
+  workstation_id        bigint(20)                                  comment '工作站ID',
+  workstation_code      varchar(64)                                 comment '工作站编号',    
+  workstation_name      varchar(255)                                comment '工作站名称',
+  process_id            bigint(20)                                  comment '工序ID',
+  process_code          varchar(64)                                 comment '工序编号',
+  process_name          varchar(255)                                comment '工序名称',
+  consume_date          datetime                                    comment '消耗日期',
+  status                varchar(64)     default 'PREPARE'           comment '单据状态',  
+  remark                varchar(500)    default ''                  comment '备注',
+  attr1                 varchar(64)     default null                comment '预留字段1',
+  attr2                 varchar(255)    default null                comment '预留字段2',
+  attr3                 int(11)         default 0                   comment '预留字段3',
+  attr4                 int(11)         default 0                   comment '预留字段4',
+  create_by             varchar(64)     default ''                  comment '创建者',
+  create_time 	        datetime                                    comment '创建时间',
+  update_by             varchar(64)     default ''                  comment '更新者',
+  update_time           datetime                                    comment '更新时间',
+  primary key (record_id)
+) engine=innodb auto_increment=200 comment = '物料消耗记录表';
 
 
-
+-- ----------------------------
+-- 11、物料消耗记录行表
+-- ----------------------------
+drop table if exists wm_item_consume_line;
+create table wm_item_consume_line (
+  line_id               bigint(20)      not null auto_increment     comment '行ID',
+  record_id             bigint(20)                                  comment '消耗记录ID',
+  material_stock_id     bigint(20)                                  comment '库存ID',
+  item_id               bigint(20)      not null                    comment '产品物料ID',
+  item_code             varchar(64)                                 comment '产品物料编码',
+  item_name             varchar(255)                                comment '产品物料名称',
+  specification         varchar(500)                                comment '规格型号',
+  unit_of_measure       varchar(64)                                 comment '单位',
+  quantity_consume      double(12,2)    not null                    comment '消耗数量',
+  batch_code            varchar(255)                                comment '领料批次号',
+  warehouse_id          bigint(20)                                  comment '仓库ID',
+  warehouse_code        varchar(64)                                 comment '仓库编码',
+  warehouse_name        varchar(255)                                comment '仓库名称',
+  location_id           bigint(20)                                  comment '库区ID',
+  location_code         varchar(64)                                 comment '库区编码',
+  location_name         varchar(255)                                comment '库区名称',
+  area_id               bigint(20)                                  comment '库位ID',
+  area_code             varchar(64)                                 comment '库位编码',
+  area_name             varchar(255)                                comment '库位名称',   
+  remark                varchar(500)    default ''                  comment '备注',
+  attr1                 varchar(64)     default null                comment '预留字段1',
+  attr2                 varchar(255)    default null                comment '预留字段2',
+  attr3                 int(11)         default 0                   comment '预留字段3',
+  attr4                 int(11)         default 0                   comment '预留字段4',
+  create_by             varchar(64)     default ''                  comment '创建者',
+  create_time 	        datetime                                    comment '创建时间',
+  update_by             varchar(64)     default ''                  comment '更新者',
+  update_time           datetime                                    comment '更新时间',
+  primary key (line_id)
+) engine=innodb auto_increment=200 comment = '物料消耗记录行表';
 
 
 
