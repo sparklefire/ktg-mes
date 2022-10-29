422b63688d0b9eb3bec60cf69607983814f83490
DESKTOP-J7ED0MB\yinjinlu <411641505@qq.com>
2022-10-29 23:00:17 +0800

    销售退货

--- ktg-mes/src/main/java/com/ktg/mes/wm/controller/WmRtSalseLineController.java
+++ ktg-mes/src/main/java/com/ktg/mes/wm/controller/WmRtSalseLineController.java
@@ -2,6 +2,14 @@ package com.ktg.mes.wm.controller;
 
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ktg.common.utils.StringUtils;
+import com.ktg.mes.wm.domain.WmStorageArea;
+import com.ktg.mes.wm.domain.WmStorageLocation;
+import com.ktg.mes.wm.domain.WmWarehouse;
+import com.ktg.mes.wm.service.IWmStorageAreaService;
+import com.ktg.mes.wm.service.IWmStorageLocationService;
+import com.ktg.mes.wm.service.IWmWarehouseService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,6 +41,15 @@ public class WmRtSalseLineController extends BaseController
 {
     @Autowired
     private IWmRtSalseLineService wmRtSalseLineService;
+
+    @Autowired
+    private IWmWarehouseService wmWarehouseService;
+
+    @Autowired
+    private IWmStorageLocationService wmStorageLocationService;
+
+    @Autowired
+    private IWmStorageAreaService wmStorageAreaService;
 
     /**
      * 查询产品销售退货行列表
@@ -77,6 +94,21 @@ public class WmRtSalseLineController extends BaseController
     @PostMapping
     public AjaxResult add(@RequestBody WmRtSalseLine wmRtSalseLine)
     {
+        if(StringUtils.isNotNull(wmRtSalseLine.getWarehouseId())){
+            WmWarehouse warehouse = wmWarehouseService.selectWmWarehouseByWarehouseId(wmRtSalseLine.getWarehouseId());
+            wmRtSalseLine.setWarehouseCode(warehouse.getWarehouseCode());
+            wmRtSalseLine.setWarehouseName(warehouse.getWarehouseName());
+        }
+        if(StringUtils.isNotNull(wmRtSalseLine.getLocationId())){
+            WmStorageLocation location = wmStorageLocationService.selectWmStorageLocationByLocationId(wmRtSalseLine.getLocationId());
+            wmRtSalseLine.setLocationCode(location.getLocationCode());
+            wmRtSalseLine.setLocationName(location.getLocationName());
+        }
+        if(StringUtils.isNotNull(wmRtSalseLine.getAreaId())){
+            WmStorageArea area = wmStorageAreaService.selectWmStorageAreaByAreaId(wmRtSalseLine.getAreaId());
+            wmRtSalseLine.setAreaCode(area.getAreaCode());
+            wmRtSalseLine.setAreaName(area.getAreaName());
+        }
         return toAjax(wmRtSalseLineService.insertWmRtSalseLine(wmRtSalseLine));
     }
 
@@ -88,6 +120,21 @@ public class WmRtSalseLineController extends BaseController
     @PutMapping
     public AjaxResult edit(@RequestBody WmRtSalseLine wmRtSalseLine)
     {
+        if(StringUtils.isNotNull(wmRtSalseLine.getWarehouseId())){
+            WmWarehouse warehouse = wmWarehouseService.selectWmWarehouseByWarehouseId(wmRtSalseLine.getWarehouseId());
+            wmRtSalseLine.setWarehouseCode(warehouse.getWarehouseCode());
+            wmRtSalseLine.setWarehouseName(warehouse.getWarehouseName());
+        }
+        if(StringUtils.isNotNull(wmRtSalseLine.getLocationId())){
+            WmStorageLocation location = wmStorageLocationService.selectWmStorageLocationByLocationId(wmRtSalseLine.getLocationId());
+            wmRtSalseLine.setLocationCode(location.getLocationCode());
+            wmRtSalseLine.setLocationName(location.getLocationName());
+        }
+        if(StringUtils.isNotNull(wmRtSalseLine.getAreaId())){
+            WmStorageArea area = wmStorageAreaService.selectWmStorageAreaByAreaId(wmRtSalseLine.getAreaId());
+            wmRtSalseLine.setAreaCode(area.getAreaCode());
+            wmRtSalseLine.setAreaName(area.getAreaName());
+        }
         return toAjax(wmRtSalseLineService.updateWmRtSalseLine(wmRtSalseLine));
     }
 
